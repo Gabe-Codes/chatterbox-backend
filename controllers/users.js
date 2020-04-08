665b504917c3826b6728850fc578da9ee9be6d55
Gabriel Russell <gabrielrussell@Gabriels-MacBook-Air.local>
2020-04-08 14:05:42 -0500

    creates user model and routes

---
+++ controllers/users.js
@@ -0,0 +1,36 @@
+const User = require('../models/user');
+
+module.exports = {
+	index,
+	create,
+	show,
+	delete: deleteUser,
+	update,
+};
+
+async function index(req, res) {
+	const users = await User.find({});
+	res.status(200).json(users);
+}
+
+async function create(req, res) {
+	const user = await User.create(req.body);
+	res.status(201).json(user);
+}
+
+async function show(req, res) {
+	const user = await User.findById(req.params.id);
+	res.status(200).json(user);
+}
+
+async function update(req, res) {
+	const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	});
+	res.status(200).json(updatedUser);
+}
+
+async function deleteUser(req, res) {
+	const deletedUser = await User.findByIdAndRemove(req.params.id);
+	res.status(200).json(deletedUser);
+}
