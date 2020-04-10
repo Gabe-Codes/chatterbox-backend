3c61d616e853ae449c1590d488ca6225999eab5b
Gabriel Russell <gabrielrussell@Gabriels-MacBook-Air.local>
2020-04-09 22:00:42 -0500

    adds user auth

--- controllers/users.js
+++ controllers/users.js
@@ -1,26 +1,47 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
 
 module.exports = {
+	signup,
+	login,
 	index,
-	create,
-	show,
 	delete: deleteUser,
 	update,
 };
 
+async function signup(req, res) {
+	const user = new User(req.body);
+	try {
+		await user.save();
+		const token = createJWT(user);
+		res.json({ token });
+	} catch (err) {
+		// Probably a duplicate email
+		res.status(400).json(err);
+	}
+}
+
+async function login(req, res) {
+	try {
+		const user = await User.findOne({ username: req.body.username });
+		if (!user) return res.status(401).json({ err: 'bad credentials' });
+		user.comparePassword(req.body.pw, (err, isMatch) => {
+			if (isMatch) {
+				const token = createJWT(user);
+				res.json({ token });
+			} else {
+				return res.status(401).json({ err: 'bad credentials' });
+			}
+		});
+	} catch (err) {
+		return res.status(401).json(err);
+	}
+}
+
 async function index(req, res) {
 	const users = await User.find({});
 	res.status(200).json(users);
-}
-
-async function create(req, res) {
-	const user = await User.create(req.body);
-	res.status(201).json(user);
-}
-
-async function show(req, res) {
-	const user = await User.findById(req.params.id);
-	res.status(200).json(user);
 }
 
 async function update(req, res) {
@@ -33,4 +54,14 @@ async function update(req, res) {
 async function deleteUser(req, res) {
 	const deletedUser = await User.findByIdAndRemove(req.params.id);
 	res.status(200).json(deletedUser);
+}
+
+/*----- Helper Functions -----*/
+
+function createJWT(user) {
+	return jwt.sign(
+		{ user }, // data payload
+		SECRET,
+		{ expiresIn: '24h' }
+	);
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const usersCtrl = require('../../controllers/users');
 
 router.get('/', usersCtrl.index);
-router.get('/:id', usersCtrl.show);
 
-router.post('/', usersCtrl.create)
+router.post('/signup', usersCtrl.signup);
+router.post('/login', usersCtrl.login);
 router.delete('/:id', usersCtrl.delete)
 router.put('/:id', usersCtrl.update)
 
